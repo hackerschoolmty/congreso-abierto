ec362184c39fb381e8aa6b462268f774035525f5
GerryITC <gerry.rendon@gmail.com>
2016-03-05 13:43:18 -0600

    Adds comission_topic model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305192720) do
+ActiveRecord::Schema.define(version: 20160305194247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comission_topics", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
---
+++ spec/factories/comission_topics.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :comission_topic do
+    name "MyString"
+    description "MyText"
+  end
+end
