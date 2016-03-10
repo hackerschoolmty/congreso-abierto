551def87e2bfa70a8032fb7f2d377bfa0f800a9e
walreyes <wal.reyes.11@gmail.com>
2016-03-09 19:36:22 -0600

    Adds status field to users
    And defines routes for activate and desactivate observers

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :rememberable, :trackable, :validatable
 
   enum role: [ :observer, :root ]
+  enum status: [:active, :desactive]
 
   scope :observers, -> { where(role: "observer") }
 end
