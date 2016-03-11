8909819077669072d3bb65b72e6b42e1ccfe8975
Juan Carlos Mercado <barkatu.83@gmail.com>
2016-03-10 23:53:32 -0600

    Adds admin layout and basic left menu in layout, adds admin section link in account dropdown

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,8 @@
 class Admin::BaseController < ApplicationController
   before_action :authenticate_user!, :root_user_mandatory!
 
+  layout 'admin'
+
   def root_user_mandatory!
     unless current_user.root?
       sign_out_all_scopes
